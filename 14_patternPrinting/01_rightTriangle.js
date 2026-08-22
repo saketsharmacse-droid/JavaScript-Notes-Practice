e5955dc42fb87403e10322512a57a10f676636b4
Saket Sharma <saket.sharma.cse@gmail.com>
2026-08-22 08:57:44 +0530

    Built a right triangle pattern using stars

---
+++ 14_patternPrinting/01_rightTriangle.js
@@ -0,0 +1,17 @@
+//yaha pe right side triangle using stars print krunga, on the console.
+
+for(let i= 1; i <= 5; i++){
+    // console.log(i);
+    let stars = "";
+    for(let j =1; j<=i; j++){
+        // console.log(i,j);
+        stars += "* ";
+    }
+    console.log(stars);
+}
+
+// * 
+// * * 
+// * * * 
+// * * * * 
+// * * * * * 
