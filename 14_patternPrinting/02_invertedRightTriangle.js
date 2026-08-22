280dbd95ac052abb0faa3bfa02e0d7f33b8ff15f
Saket Sharma <saket.sharma.cse@gmail.com>
2026-08-22 09:04:08 +0530

    Added an inverted right triangle printed pattern

---
+++ 14_patternPrinting/02_invertedRightTriangle.js
@@ -0,0 +1,21 @@
+//i'll make an inverted right triangle.
+
+for(let i = 5; i>=1; i--){
+    // console.log(i);
+    let stars = ""
+    for(j=i; j>=1; j--){
+        // console.log(i,j);
+        stars += "* ";
+    }
+    console.log(stars);
+}
+
+
+
+/*
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+*/
